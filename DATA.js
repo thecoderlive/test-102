edc5a1284249229609878f9ae5a63fe3f388fdb1
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-04 03:47:42 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "091d2238-6456-49e5-9d72-0825fb4170c6",
+    "home_feed": [
+        {
+            "id": "1621c26a-5cfa-4aa2-a475-482d070dde4a",
+            "artist_name": "some",
+            "home_carousel": [
+                {
+                    "id": "4625a260-854a-4282-bb35-ded43d4c11e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png"
+                },
+                {
+                    "id": "72a83886-f6fd-42c3-8113-832cb6860ba5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                },
+                {
+                    "id": "ee395a6a-ebac-41f2-bb64-6decdd987747",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "18ba47b0-766f-47c3-af5a-6b47def6af83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "c8ca9c06-ae94-45dd-83b5-6f6eb3a30d48",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                }
+            ],
+            "art_caption": "player",
+            "art_description": "Owner remain born kitchen grow would air. One big detail form floor tonight number ability."
+        },
+        {
+            "id": "b66a66be-fb4e-4f96-91ea-61e235ac8de2",
+            "artist_name": "together",
+            "home_carousel": [
+                {
+                    "id": "11ce76a5-a377-487d-acfc-c7e7aaa05d66",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "c90ef026-740c-4ffd-9aec-8beb73be3f7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                },
+                {
+                    "id": "61e08e3e-6fe9-4e6e-9292-de11380f4c1e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                },
+                {
+                    "id": "d8b4c6cc-eb95-4fb0-bc48-81589408acaa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "39046e83-1d84-4eb6-a913-2754c71726bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                }
+            ],
+            "art_caption": "big",
+            "art_description": "Large dream nature old attention bar. Business just oil respond pretty establish world."
+        },
+        {
+            "id": "af3d5264-a956-4183-b7aa-b424da031cdf",
+            "artist_name": "indeed",
+            "home_carousel": [
+                {
+                    "id": "b18ad299-b2f4-42b5-8633-e8d9fbb954e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "7b2a62c4-e09f-49af-a6fe-edf1cdfb4eaa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                },
+                {
+                    "id": "f8adc7d0-9832-492b-93e7-c10fac61cef5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "e03e9124-151f-4b94-aaaf-9f1f4b1e1807",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "a9227f91-7e8c-4e8f-8a17-03b331be88c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png"
+                }
+            ],
+            "art_caption": "west",
+            "art_description": "Themselves top determine left. Remain TV around church. Happy poor financial structure order."
+        },
+        {
+            "id": "aa330a66-2184-410f-9261-a71f303ac7d3",
+            "artist_name": "she",
+            "home_carousel": [
+                {
+                    "id": "43acb35b-b4b9-49a7-b399-a0a7233fca0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "84d4b8c1-0a5b-4be4-a1c0-e3ebc1a6ed4f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "5e79b677-73d8-466a-9b0d-a1304e8bc062",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "8157563c-ae9a-4dc1-b4e9-d2ea6da83d00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "db2fca5e-e40f-4aab-9808-44fc93fa2b20",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                }
+            ],
+            "art_caption": "see",
+            "art_description": "Table whether wait painting majority once. Feeling they clearly home activity. Seem be consumer blue crime couple pattern."
+        },
+        {
+            "id": "ed3820e3-0c9a-4a7a-a05b-6d47527673ce",
+            "artist_name": "true",
+            "home_carousel": [
+                {
+                    "id": "bde6f86e-c965-4671-a6c4-5f092e50d425",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                },
+                {
+                    "id": "74cc0a48-2736-41a8-8107-8db010f83db1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/341.png"
+                },
+                {
+                    "id": "5fc76644-0bd9-4ebd-9d06-2680b7d18330",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                },
+                {
+                    "id": "bc493ff6-d957-42f7-87a0-33e2cdbbee00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "0f1461f5-fd41-432c-860c-1f26b26b0f3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png"
+                }
+            ],
+            "art_caption": "charge",
+            "art_description": "Art character gas whole stock item reflect soon. Bag section news mean letter spend. Mrs make conference know."
+        }
+    ]
+}
